3e710b54b4e7f323773541201774c22a3cef0480
Lior Cohen <lior1cc@gmail.com>
2024-12-24 22:51:10 +0200

    Update content.js

--- content.js
+++ content.js
@@ -1,1 +1,16 @@
 
+(() => {
+  const domain = window.location.hostname.split('.')[1]; // Extract domain name without subdomains
+
+  // Extract title and chapter from URL
+  const urlParts = window.location.pathname.split('/');
+  const title = urlParts[2] || ""; // Assuming title is the second segment in the path
+  const chapterMatch = urlParts.find(part => part.match(/chapter-(\d+)/i));
+  const chapter = chapterMatch ? chapterMatch.match(/chapter-(\d+)/i)[1] : null;
+
+  chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.action === "getPageData") {
+      sendResponse({ domain, title, chapter });
+    }
+  });
+})();
