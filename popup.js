f33224edeb6c92229b2ef24bfa9fdc263eba5806
Lior Cohen <lior1cc@gmail.com>
2024-12-24 22:27:55 +0200

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,1 +1,22 @@
+document.getElementById("search-button").addEventListener("click", () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.tabs.sendMessage(tabs[0].id, { action: "getPageData" }, (response) => {
+      if (!response) {
+        showError();
+        return;
+      }
 
+      const { title, chapter, domain } = response;
+      chrome.runtime.sendMessage(
+        { action: "searchAlternatives", title, chapter },
+        (res) => {
+          if (!res || !res.success) {
+            showError();
+          } else {
+            displayResults(res.links, domain);
+          }
+        }
+      );
+    });
+  });
+});
